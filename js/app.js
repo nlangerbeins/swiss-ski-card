7ec7e9508890b3603a6890a306779d20867b5d49
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2024-02-01 00:12:25 +0100

    added navbar

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const questions = document.querySelectorAll('.question');
 
 questions.forEach((question) => {
   const btn = question.querySelector('.question-btn');
-  console.log(btn);
 
   btn.addEventListener('click', () => {
     questions.forEach((item) => {
@@ -12,4 +11,37 @@ questions.forEach((question) => {
     });
     question.classList.toggle('show-text');
   });
+});
+
+// Hamburger Button
+const hamburger = document.querySelector('.hamburger-btn');
+const navMenu = document.querySelector('.nav-links');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+  navMenu.classList.toggle('open');
+});
+
+const links = document.querySelectorAll('.nav-link');
+links.forEach((link) =>
+  link.addEventListener('click', function () {
+    hamburger.classList.remove('open');
+    navMenu.classList.remove('open');
+  })
+);
+
+document.querySelector('.nav-logo').addEventListener('click', () => {
+  hamburger.classList.remove('open');
+  navMenu.classList.remove('open');
+});
+
+// Header Background change on scroll
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= window.innerHeight - 700) {
+    document.querySelector('.navbar').classList.add('active');
+    document.querySelector('.nav-logo').classList.add('logo-active');
+  } else {
+    document.querySelector('.navbar').classList.remove('active');
+    document.querySelector('.nav-logo').classList.remove('logo-active');
+  }
 });
