00e70da37a72879a0291a3fef64e1ff67e889b43
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2024-02-02 17:31:24 +0100

    added numbers and slides functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+// Numbers
+const items = [...document.querySelectorAll('#number')];
+console.log(items);
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initial = 0;
+
+  const increaseCount = setInterval(() => {
+    initial += increment;
+
+    if (initial > value) {
+      el.textContent = value;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = initial;
+  }, 10);
+};
+
+items.forEach((i) => updateCount(i));
+
+// Slides
+const slides = document.querySelectorAll('.single-slide');
+slides.forEach((el) => {
+  el.addEventListener('mouseover', () => {
+    removeFocus();
+    el.classList.add('selected');
+  });
+
+  removeFocus = () => {
+    slides.forEach((item) => {
+      item.classList.remove('selected');
+    });
+  };
+});
+
+// Questions
 const questions = document.querySelectorAll('.question');
 
 questions.forEach((question) => {
@@ -35,7 +75,6 @@ document.querySelector('.nav-logo').addEventListener('click', () => {
 });
 
 // Header Background change on scroll
-
 window.addEventListener('scroll', () => {
   if (window.scrollY >= window.innerHeight - 700) {
     document.querySelector('.navbar').classList.add('active');
